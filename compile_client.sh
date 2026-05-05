3ad06deba95d1cae15be34cd8475e95a90a343f3
4vun1t <felix.altenbach1999@gmail.com>
2026-05-05 11:21:10 +0000

    Add compile script for client compilation

---
+++ compile_client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd artirat-client
+cargo build --lib --release --target armv7-linux-androideabi
+cargo build --release --target armv7-linux-androideabi
+cargo build --release --lib --target aarch64-linux-android
+cargo build --release --target aarch64-linux-android
+cargo build --target x86_64-pc-windows-gnu --lib --release
+cargo build --target x86_64-pc-windows-gnu --release
+cargo build --release --lib --target x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl
+cargo build --release --target i586-unknown-linux-musl --lib
+cargo build --release --target i586-unknown-linux-musl
